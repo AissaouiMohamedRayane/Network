9fd63e48cb781a3d1fc81f56203e92f4f1917fa6
Mohaaammeed <your.email@example.com>
2024-02-10 23:42:11 +0100

    Poste model DONE

--- network/models.py
+++ network/models.py
@@ -1,4 +1,61 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, username, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=20)
+    liked_postes=models.ManyToManyField('Poste',related_name='liking_users', blank=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = CustomUserManager()
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+
+    def __str__(self):
+        return self.email
+
+class Comment(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    text=models.CharField( max_length=1000)
+    likes=models.IntegerField(default=0)
+    replies=models.ManyToManyField('self', blank=True)
+    def __str__(self):
+        return f"{self.text}"
+
+class Poste(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
+    text=models.CharField(max_length=1000)
+    likes=models.IntegerField(default=0)
+    comments=models.ManyToManyField(Comment, blank=True)
+    
